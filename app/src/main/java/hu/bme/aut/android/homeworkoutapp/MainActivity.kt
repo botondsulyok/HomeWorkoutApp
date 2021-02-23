fdcf9981d7b95fbdfe3da0303359f176e6e52c03
botondsulyok <59982368+botondsulyok@users.noreply.github.com>
2021-02-23 20:59:52 +0100

    data binding, toolbar

--- app/src/main/java/hu/bme/aut/android/homeworkoutapp/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/android/homeworkoutapp/MainActivity.kt
@@ -1,26 +1,53 @@
 package hu.bme.aut.android.homeworkoutapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import hu.bme.aut.android.homeworkoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setSupportActionBar(binding.toolbar)
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_workouts, R.id.navigation_exercises, R.id.navigation_plans))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_main_settings -> {
+                // TODO show a popup menu, or a dialog fragment
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
 }
--- app/src/main/java/hu/bme/aut/android/homeworkoutapp/ui/workouts/WorkoutsFragment.kt
+++ app/src/main/java/hu/bme/aut/android/homeworkoutapp/ui/workouts/WorkoutsFragment.kt
@@ -1,23 +1,49 @@
 package hu.bme.aut.android.homeworkoutapp.ui.workouts
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
 import co.zsmb.rainbowcake.dagger.getViewModelFromFactory
 import co.zsmb.rainbowcake.extensions.exhaustive
-import hu.bme.aut.android.homeworkoutapp.R
-import kotlinx.android.synthetic.main.fragment_workouts.*
+import hu.bme.aut.android.homeworkoutapp.MainActivity
+import hu.bme.aut.android.homeworkoutapp.databinding.FragmentWorkoutsBinding
 
 class WorkoutsFragment : RainbowCakeFragment<WorkoutsViewState, WorkoutsViewModel>() {
 
     override fun provideViewModel() = getViewModelFromFactory()
-    override fun getViewResource() = R.layout.fragment_workouts
+    override fun getViewResource() = 0
+
+    private var _binding: FragmentWorkoutsBinding? = null
+    private val binding get() = _binding!!
+
+    private var mainActivity: MainActivity? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWorkoutsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // TODO Setup views
 
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mainActivity = activity as? MainActivity
     }
 
     override fun onStart() {
@@ -28,10 +54,10 @@ class WorkoutsFragment : RainbowCakeFragment<WorkoutsViewState, WorkoutsViewMode
     override fun render(viewState: WorkoutsViewState) {
         when(viewState) {
             is WorkoutsLoading -> {
-                progressBar.visibility = View.VISIBLE
+                binding.progressBar.visibility = View.VISIBLE
             }
             is WorkoutsReady -> {
-                progressBar.visibility = View.GONE
+                binding.progressBar.visibility = View.GONE
             }
         }.exhaustive
 
