40d45450e2c706835dcd7e603b40a12915308535
botondsulyok <59982368+botondsulyok@users.noreply.github.com>
2021-03-02 22:34:57 +0100

    timestamp

--- app/src/main/java/hu/bme/aut/android/homeworkoutapp/data/firebase/FireBaseDataSource.kt
+++ app/src/main/java/hu/bme/aut/android/homeworkoutapp/data/firebase/FireBaseDataSource.kt
@@ -3,13 +3,17 @@ package hu.bme.aut.android.homeworkoutapp.data.firebase
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import hu.bme.aut.android.homeworkoutapp.data.ResultFailure
 import hu.bme.aut.android.homeworkoutapp.data.Result
 import hu.bme.aut.android.homeworkoutapp.data.ResultSuccess
+import hu.bme.aut.android.homeworkoutapp.data.models.FirebaseWorkout
 import hu.bme.aut.android.homeworkoutapp.domain.models.DomainWorkout
+import hu.bme.aut.android.homeworkoutapp.ui.newworkout.models.UiNewWorkout
 import kotlinx.coroutines.tasks.await
 import java.util.concurrent.ExecutionException
 import javax.inject.Inject
@@ -31,9 +35,7 @@ class FireBaseDataSource @Inject constructor() {
             auth.signInWithCredential(credential).await()
             userId = auth.currentUser?.uid.toString()
             ResultSuccess(Unit)
-        } catch (e: ExecutionException) {
-            ResultFailure(e)
-        } catch (e: InterruptedException) {
+        } catch (e: Exception) {
             ResultFailure(e)
         }
     }
@@ -52,16 +54,15 @@ class FireBaseDataSource @Inject constructor() {
             .collection("userdata")
             .document(userId)
             .collection("workouts")
+            .orderBy("creation", Query.Direction.DESCENDING)
 
         return try {
             val workoutsSnapshot = workoutsRef.get().await()
             val workouts = workoutsSnapshot.documents.map {
-                it.toObject<DomainWorkout>() ?: DomainWorkout()
+                it.toObject<FirebaseWorkout>()?.toDomainWorkout() ?: DomainWorkout()
             }
             ResultSuccess(workouts)
-        } catch (e: ExecutionException) {
-            ResultFailure(e)
-        } catch (e: InterruptedException) {
+        } catch (e: Exception) {
             ResultFailure(e)
         }
 
@@ -74,35 +75,47 @@ class FireBaseDataSource @Inject constructor() {
             .collection("workouts")
             .document()
 
-        val newWorkout = workout.copy(id = newWorkoutRef.id)
+        val newWorkout = workout.toFirebaseWorkout(newWorkoutRef.id)
 
         return try {
             newWorkoutRef.set(newWorkout).await()
             ResultSuccess(Unit)
-        } catch (e: ExecutionException) {
-            ResultFailure(e)
-        } catch (e: InterruptedException) {
+        } catch (e: Exception) {
             ResultFailure(e)
         }
 
     }
 
     suspend fun deleteWorkout(workout: DomainWorkout): Result<Unit, Exception> {
+        val deleteWorkout = workout.toFirebaseWorkout()
+
         val deleteWorkoutRef = db
             .collection("userdata")
             .document(userId)
             .collection("workouts")
-            .document(workout.id)
+            .document(deleteWorkout.id)
 
         return try {
             deleteWorkoutRef.delete().await()
             ResultSuccess(Unit)
-        } catch (e: ExecutionException) {
-            ResultFailure(e)
-        } catch (e: InterruptedException) {
+        } catch (e: Exception) {
             ResultFailure(e)
         }
     }
 
 
+}
+
+private fun DomainWorkout.toFirebaseWorkout(id: String = this.id): FirebaseWorkout {
+    return FirebaseWorkout(
+        id = id,
+        name = name
+    )
+}
+
+private fun FirebaseWorkout.toDomainWorkout(): DomainWorkout {
+    return DomainWorkout(
+        id = id,
+        name = name
+    )
 }
