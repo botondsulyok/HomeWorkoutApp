12b8b38b5502d92f124562859d4996fa79334770
botondsulyok <59982368+botondsulyok@users.noreply.github.com>
2021-03-05 14:22:42 +0100

    a

--- app/src/main/java/hu/bme/aut/android/homeworkoutapp/ui/newexercise/models/UiNewExercise.kt
+++ app/src/main/java/hu/bme/aut/android/homeworkoutapp/ui/newexercise/models/UiNewExercise.kt
@@ -1,5 +1,7 @@
 package hu.bme.aut.android.homeworkoutapp.ui.newexercise.models
 
 data class UiNewExercise(
-        val name: String = ""
+        val name: String = "",
+        val isRepsAdded: Boolean = true,
+        val repsOrDurationValue: Int = 0
 )
