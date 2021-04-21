bc92c6f5563ffdbdc127364e29d6a37769a5bfbb
botondsulyok <botond.sulyok99@gmail.com>
2021-04-21 16:19:41 +0200

    Create a calendar in PlansFragment

--- app/src/main/java/hu/bme/aut/android/homeworkoutapp/ui/plans/PlansFragment.kt
+++ app/src/main/java/hu/bme/aut/android/homeworkoutapp/ui/plans/PlansFragment.kt
@@ -5,38 +5,239 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.children
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import hu.bme.aut.android.homeworkoutapp.MainActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.model.CalendarMonth
+import com.kizitonwose.calendarview.model.DayOwner
+import com.kizitonwose.calendarview.ui.DayBinder
+import com.kizitonwose.calendarview.ui.MonthHeaderFooterBinder
+import com.kizitonwose.calendarview.ui.ViewContainer
 import hu.bme.aut.android.homeworkoutapp.R
+import hu.bme.aut.android.homeworkoutapp.databinding.*
+import hu.bme.aut.android.homeworkoutapp.utils.daysOfWeekFromLocale
+import hu.bme.aut.android.homeworkoutapp.utils.setTextColorRes
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+data class Plans(val id: String, val text: String, val date: LocalDate)
+
+class Example3EventsAdapter(val onClick: (Plans) -> Unit) :
+    RecyclerView.Adapter<Example3EventsAdapter.ViewHolder>() {
+
+    val events = mutableListOf<Plans>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = PlansEventItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.bind(events[position])
+    }
+
+    override fun getItemCount(): Int = events.size
+
+    inner class ViewHolder(private val binding: PlansEventItemViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            itemView.setOnClickListener {
+                onClick(events[bindingAdapterPosition])
+            }
+        }
+
+        fun bind(event: Plans) {
+            binding.itemEventText.text = event.text
+        }
+    }
+}
 
 class PlansFragment : Fragment() {
 
-    private lateinit var plansViewModel: PlansViewModel
+    private var _binding: FragmentPlansBinding? = null
+    private val binding get() = _binding!!
 
-    private var mainActivity: MainActivity? = null
-
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        plansViewModel =
-                ViewModelProvider(this).get(PlansViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_plans, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        plansViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    private val eventsAdapter = Example3EventsAdapter {
+        AlertDialog.Builder(requireContext())
+            .setMessage("R.string.example_3_dialog_delete_confirmation")
+            .setPositiveButton("R.string.delete") { _, _ ->
+                deleteEvent(it)
+            }
+            .setNegativeButton("R.string.close", null)
+            .show()
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        mainActivity = activity as? MainActivity
-        mainActivity?.supportActionBar?.show()
-        mainActivity?.binding?.navView?.visibility = View.VISIBLE
+    private var selectedDate: LocalDate? = null
+    private val today = LocalDate.now()
+
+    private val titleSameYearFormatter = DateTimeFormatter.ofPattern("MMMM")
+    private val titleFormatter = DateTimeFormatter.ofPattern("MMM yyyy")
+    private val selectionFormatter = DateTimeFormatter.ofPattern("d MMM yyyy")
+    private val events = mutableMapOf<LocalDate, List<Plans>>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPlansBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        binding.exThreeRv.apply {
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+            adapter = eventsAdapter
+            addItemDecoration(DividerItemDecoration(requireContext(), RecyclerView.VERTICAL))
+        }
+
+        val daysOfWeek = daysOfWeekFromLocale()
+        val currentMonth = YearMonth.now()
+        binding.exThreeCalendar.apply {
+            setup(currentMonth.minusMonths(10), currentMonth.plusMonths(10), daysOfWeek.first())
+            scrollToMonth(currentMonth)
+        }
+
+        if (savedInstanceState == null) {
+            binding.exThreeCalendar.post {
+                // Show today's events initially.
+                selectDate(today)
+            }
+        }
+
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            lateinit var day: CalendarDay // Will be set when this container is bound.
+            val binding = PlansCalendarDayBinding.bind(view)
+
+            init {
+                view.setOnClickListener {
+                    if (day.owner == DayOwner.THIS_MONTH) {
+                        selectDate(day.date)
+                    }
+                }
+            }
+        }
+        binding.exThreeCalendar.dayBinder = object : DayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.day = day
+                val textView = container.binding.exThreeDayText
+                val dotView = container.binding.exThreeDotView
+
+                textView.text = day.date.dayOfMonth.toString()
+
+                if (day.owner == DayOwner.THIS_MONTH) {
+                    textView.visibility = View.VISIBLE
+                    when (day.date) {
+                        today -> {
+                            // todo 5
+                            textView.setTextColorRes(R.color.secondaryTextColorNight)
+                            textView.setBackgroundResource(R.drawable.plans_today_bg)
+                            dotView.visibility = View.INVISIBLE
+                        }
+                        selectedDate -> {
+                            textView.setTextColorRes(R.color.secondaryLightColorNight)
+                            textView.setBackgroundResource(R.drawable.plans_selected_bg)
+                            dotView.visibility = View.INVISIBLE
+                        }
+                        else -> {
+                            textView.setTextColorRes(R.color.primaryTextColorNight)
+                            textView.background = null
+                            dotView.isVisible = events[day.date].orEmpty().isNotEmpty()
+                        }
+                    }
+                } else {
+                    textView.visibility = View.INVISIBLE
+                    dotView.visibility = View.INVISIBLE
+                }
+            }
+        }
+
+        binding.exThreeCalendar.monthScrollListener = {
+            /*homeActivityToolbar.title = if (it.year == today.year) {
+                titleSameYearFormatter.format(it.yearMonth)
+            } else {
+                titleFormatter.format(it.yearMonth)
+            }*/
+
+            // Select the first day of the month when
+            // we scroll to a new month.
+            selectDate(it.yearMonth.atDay(1))
+        }
+
+        class MonthViewContainer(view: View) : ViewContainer(view) {
+            val legendLayout = PlansCalendarHeaderBinding.bind(view).legendLayout.root
+        }
+        binding.exThreeCalendar.monthHeaderBinder = object : MonthHeaderFooterBinder<MonthViewContainer> {
+            override fun create(view: View) = MonthViewContainer(view)
+            override fun bind(container: MonthViewContainer, month: CalendarMonth) {
+                // Setup each header day text if we have not done that already.
+                if (container.legendLayout.tag == null) {
+                    container.legendLayout.tag = month.yearMonth
+                    container.legendLayout.children.map { it as TextView }.forEachIndexed { index, tv ->
+                        tv.text = daysOfWeek[index].name.first().toString()
+                        // todo
+                        tv.setTextColorRes(R.color.primaryColor)
+                    }
+                }
+            }
+        }
+
+        binding.fabAddWorkoutToPlan.setOnClickListener {
+            Toast.makeText(requireContext(), "inputDialog.show()", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun selectDate(date: LocalDate) {
+        if (selectedDate != date) {
+            val oldDate = selectedDate
+            selectedDate = date
+            oldDate?.let { binding.exThreeCalendar.notifyDateChanged(it) }
+            binding.exThreeCalendar.notifyDateChanged(date)
+            updateAdapterForDate(date)
+        }
+    }
+
+    private fun saveEvent(text: String) {
+        if (text.isBlank()) {
+            Toast.makeText(requireContext(), "R.string.example_3_empty_input_text", Toast.LENGTH_LONG).show()
+        } else {
+            selectedDate?.let {
+                events[it] = events[it].orEmpty().plus(Plans(UUID.randomUUID().toString(), text, it))
+                updateAdapterForDate(it)
+            }
+        }
+    }
+
+    private fun deleteEvent(event: Plans) {
+        val date = event.date
+        events[date] = events[date].orEmpty().minus(event)
+        updateAdapterForDate(date)
+    }
+
+    private fun updateAdapterForDate(date: LocalDate) {
+        eventsAdapter.apply {
+            events.clear()
+            events.addAll(this@PlansFragment.events[date].orEmpty())
+            notifyDataSetChanged()
+        }
+        binding.exThreeSelectedDateText.text = selectionFormatter.format(date)
     }
 
 }
