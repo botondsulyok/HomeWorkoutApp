01e1790ef330f94e5b0e4004866510c407d9984f
botondsulyok <botond.sulyok99@gmail.com>
2021-05-01 12:02:40 +0200

    Update MainActivityTest.kt

--- app/src/androidTest/java/hu/bme/aut/android/homeworkoutapp/MainActivityTest.kt
+++ app/src/androidTest/java/hu/bme/aut/android/homeworkoutapp/MainActivityTest.kt
@@ -1,12 +1,18 @@
 package hu.bme.aut.android.homeworkoutapp
 
+import androidx.navigation.findNavController
+import androidx.test.InstrumentationRegistry.getTargetContext
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.PositionAssertions.isCompletelyAbove
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.google.common.truth.Truth
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,6 +22,12 @@ import org.junit.runner.RunWith
 class MainActivityTest {
 
     @get:Rule var activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    private fun navigateToWorkoutsFragment() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            activity.findNavController(R.id.nav_host_fragment).navigate(R.id.navigation_workouts)
+        }
+    }
 
     @Test
     fun whenMainActivityStarts_thenNavHostFragmentVisible() {
@@ -30,6 +42,37 @@ class MainActivityTest {
     @Test
     fun navHostFragmentIsAboveOfNavView() {
         onView(withId(R.id.nav_host_fragment)).check(isCompletelyAbove(withId(R.id.nav_view)))
+    }
+
+    @Test
+    fun whenActionMainSettingsMenuItemClicked_thenPopupMenuVisibleWithItems() {
+        // Given
+        navigateToWorkoutsFragment()
+
+        // When
+        onView(withId(R.id.action_main_settings)).perform(click())
+
+        // Then
+        onView(withText(R.string.menu_sign_out)).check(matches(isDisplayed()))
+        onView(withText(R.string.menu_settings)).check(matches(isDisplayed()))
+    }
+
+
+    @Test
+    fun whenSignOutMenuItemClicked_thenNavigateToWelcomeFragment() {
+        // Given
+        navigateToWorkoutsFragment()
+
+        // When
+        onView(withId(R.id.action_main_settings)).perform(click())
+        onView(withText(R.string.menu_sign_out)).perform(click())
+
+        // Then
+        activityScenarioRule.scenario.onActivity { activity ->
+            Truth.assertThat(
+                activity.findNavController(R.id.nav_host_fragment).currentDestination?.id)
+                .isEqualTo(R.id.navigation_welcome)
+        }
     }
 
 }
